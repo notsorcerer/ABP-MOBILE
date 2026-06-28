1e2944b74015157f988c2b2015f5d5b3b75235cb
notsorcerer <dewandakila@gmail.com>
2026-06-28 18:50:26 +0700

    config: update baseUrl to current network IP

--- lib/config/api_config.dart
+++ lib/config/api_config.dart
@@ -2,7 +2,7 @@ class ApiConfig {
   // Untuk Android Emulator: 'http://10.0.2.2:8000'
   // Untuk HP fisik (1 jaringan): 'http://192.168.x.x:8000'
   // Untuk iOS Simulator / Web: 'http://localhost:8000'
-  static const String baseUrl = 'http://192.168.0.110:8000';
+  static const String baseUrl = 'http://10.131.244.112:8000';
   static const String apiPrefix = '/api/';
   static const Duration timeout = Duration(seconds: 30);
 }
